fce4b33dda77ed65af4cef48b089802c3f5622ba
thesilla <maxegillman@gmail.com>
2018-11-23 00:41:31 -0500

    added bootswatch theme, configured within views

--- ticketsView.php
+++ ticketsView.php
@@ -1,28 +1,32 @@
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="styles.css">
+        <link rel="stylesheet" type="text/css" href="superhero.css">
+        <!-- <link rel="stylesheet" type="text/css" href="styles.css"> -->
     </head>
 
 
     <body>
-        <div> 
+        <div id="tickets-container">
             <!-- Table headers -->
-            <table id="displayTickets">
-                <tr>
-                    <th onclick='sortTable(0)'>Ticket ID#</th>
-                    <th onclick='sortTable(2)'>Requested By</th>
-                    <th onclick='sortTable(3)'>Submitted By</th>
-                    <th onclick='sortTable(4)'>Date Submitted</th>
-                    <th onclick='sortTable(5)'>Category</th>
-                    <th onclick='sortTable(6)'>Subject</th>
-                    <th onclick='sortTable(7)'>Details</th>
-                    <th onclick='sortTable(8)'>Priority</th>
-                    <th onclick='sortTable(9)'>Assigned To</th>
-                    <th onclick='sortTable(10)'>Order ID</th>
-                    <th onclick='sortTable(11)'>Status</th>
-                    <th onclick='sortTable(12)'>Completed?</th>
-                    <th onclick='sortTable(13)'>Date Completed</th>
-                    <th></th>
+            <!--<table id="displayTickets">-->
+            <table class="table table-hover">
+                <tr><thead>
+                <th onclick='sortTable(0)'>Ticket ID#</th>
+                <th onclick='sortTable(2)'>Requested By</th>
+                <th onclick='sortTable(3)'>Submitted By</th>
+                <th onclick='sortTable(4)'>Date Submitted</th>
+                <th onclick='sortTable(5)'>Category</th>
+                <th onclick='sortTable(6)'>Subject</th>
+                <th onclick='sortTable(7)'>Details</th>
+                <th onclick='sortTable(8)'>Priority</th>
+                <th onclick='sortTable(9)'>Assigned To</th>
+                <th onclick='sortTable(10)'>Order ID</th>
+                <th onclick='sortTable(11)'>Status</th>
+                <th onclick='sortTable(12)'>Completed?</th>
+                <th onclick='sortTable(13)'>Date Completed</th>
+                </thead>
+                <th></th>
+                <th></th>
                 </tr>
 
                 <?php
@@ -30,7 +34,24 @@
 
                 foreach ($allTickets as $ticket) {
 
-                    echo "<tr>";
+                    if ($ticket->getPriority() == 1) {
+
+                        echo "<tr class='table-danger'>";
+                    } else {
+
+                        if ($ticket->getPriority() == 2) {
+
+
+                            echo "<tr class='table-warning'>";
+                        } else {
+
+
+                            echo "<tr class='table-primary'>";
+                        }
+                    }
+
+
+
 
                     // display data
                     echo "<td>" . $ticket->getid() . "</td>";
@@ -47,16 +68,17 @@
                     echo "<td>" . $ticket->getCompleted() . "</td>";
                     echo "<td>" . $ticket->getDateResolved() . "</td>";
                     $id = $ticket->getid();
-                    echo "<td> <form action = 'ticketDetailController.php' method = 'get'><input name = 'ticketno' id = 'editTicket' type = 'hidden' value = $id> <input type = 'submit' name = 'submit1' value = 'Details'></form> </td>";
 
+                    echo "<td> <form action = 'ticketDetailController.php' method = 'get'><input name = 'ticketno' id = 'editTicket' type = 'hidden' value = $id> <input class='btn btn-success' type = 'submit' name = 'submit1' value = 'Details'></form> </td>";
+                    echo "<td> <form action = 'ticketDetailController.php' method = 'get'><input name = 'ticketno' id = 'editTicket' type = 'hidden' value = $id> <input class='btn btn-danger' type = 'submit' name = 'delete-ticket' value = 'Delete'></form> </td>";
                     echo "</tr>";
                 }
                 ?>
 
             </table>
 
-            
+
         </div>
-        
-   
+
+
 
